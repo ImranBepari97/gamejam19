733695649291b78ddce1eae98fd94794f25aec0b
Thomas Sarno <TJSarno98@gmail.com>
2019-01-26 11:36:07 +0000

    Added mute buttons on menu

--- Assets/Scripts/SfxPlayer.cs
+++ Assets/Scripts/SfxPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SfxPlayer : MonoBehaviour
 {
-    public bool IsMuted;
+    private bool IsMuted;
     private AudioSource[] SfxPlayers;
     // Start is called before the first frame update
     void Awake()
