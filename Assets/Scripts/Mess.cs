bf5c24fadc884a6460da0a5cbfc155acc8d0cdfd
lacia9419 <46899186+lacia9419@users.noreply.github.com>
2019-01-27 11:17:26 +0000

    Updated Sound, UI and Carpet

--- Assets/Scripts/Mess.cs
+++ Assets/Scripts/Mess.cs
@@ -9,6 +9,8 @@ public enum FloorTypes
 
 public class Mess : Interactable
 {
+    public AudioClip CleanMessEndSfx;
+    public AudioClip CleaningSfx;
     public float cleanUpTime;
     public int messPoints;
     public ToolName recieveBonusFrom;
@@ -19,17 +21,37 @@ public class Mess : Interactable
     public LayerMask WhatIsGround;
     public float TiledMultiplier;
     public float CarpetMultiplier;
+    public bool OnSide;
+    public bool OnOtherSide;
+    private SfxPlayer Sfx;
     
     // Start is called before the first frame update
     void Start()
     {
+        Sfx = GameObject.Find("SfxPlayer").GetComponent<SfxPlayer>();
         GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 		base.Start();
+        if (TiledMultiplier == 0) TiledMultiplier = 1;
+        if (CarpetMultiplier == 0) CarpetMultiplier = 1;
+        if (bonusMultiplier == 0) bonusMultiplier = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (OnSide)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * 50, Color.yellow);
+        }
+        else if (OnOtherSide)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 50, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 50, Color.yellow);
+        }
 
     }
     
@@ -40,7 +62,58 @@ public class Mess : Interactable
     /// <param name="thisplayer"></param>
     public void CleanMess(PlayerInteract thisplayer)
     {
-       // var hit = Physics.Raycast(transform.position, Vector3.down, WhatIsGround);
+        RaycastHit hit;
+       // print("ray");
+        if (OnSide)
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, Mathf.Infinity, WhatIsGround))
+            {
+                if (hit.transform.gameObject.CompareTag("Carpet"))
+                {
+                    print("Carpet");
+                    cleanUpTime = cleanUpTime / CarpetMultiplier;
+                    
+                }
+                if (hit.transform.gameObject.CompareTag("Tiled"))
+                {
+                    print("Tiled");
+                    cleanUpTime = cleanUpTime / TiledMultiplier;
+                }
+            }
+        }
+        else if (OnOtherSide)
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, WhatIsGround))
+            {
+                if (hit.transform.gameObject.CompareTag("Carpet"))
+                {
+                    print("Carpet");
+                    cleanUpTime = cleanUpTime / CarpetMultiplier;
+
+                }
+                if (hit.transform.gameObject.CompareTag("Tiled"))
+                {
+                    print("Tiled");
+                    cleanUpTime = cleanUpTime / TiledMultiplier;
+                }
+            }
+        }
+        else
+        {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, WhatIsGround))
+                {
+                    
+                if (hit.transform.gameObject.CompareTag("Carpet"))
+                {
+                    print("Carpet");
+                }
+                if (hit.transform.gameObject.CompareTag("Tiled"))
+                {
+                    print("Tiled");
+                }
+            }
+        }
+        
        
 
 
@@ -62,6 +135,11 @@ public class Mess : Interactable
         player = thisplayer;
         thisplayer.gameObject.transform.GetChild(1).gameObject.SetActive(true);
         thisplayer.gameObject.transform.GetChild(1).GetComponent<Castbar>().Activated(cleanUpTime);
+        Sfx.PlaySfx(CleaningSfx);
+        if (player.currentTool != null)
+        {
+            Sfx.PlaySfx(player.currentTool.UseSfx);
+        }
     }
 
     /// <summary>
@@ -82,6 +160,8 @@ public class Mess : Interactable
             GameManager.AddCleanScore(messPoints);
         }
         player.FinishClean(); //reactivates player movement
+        Sfx.PlaySfx(CleanMessEndSfx);
+        
         //add points to global score
         //stop clean progress bar
         
@@ -92,5 +172,6 @@ public class Mess : Interactable
         transform.parent.GetChild(1).gameObject.SetActive(true);
         transform.parent.GetChild(1).transform.position = new Vector3(transform.position.x, transform.parent.GetChild(1).transform.position.y, transform.position.z);
         GameManager.AddMessScore(messPoints);
+
     }
 }
